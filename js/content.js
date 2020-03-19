055eba03cdc01d5b1653f288b00853d89f2f5bee
Epoc <contact@epoc.fr>
2020-03-19 20:31:54 +0100

    Continuation

--- js/content.js
+++ js/content.js
@@ -19,7 +19,7 @@
             console.debug('GitLab base URL:', this.baseGitLabUrl);
             console.debug('Current project ID:', this.currentProjectId);
 
-            this.refreshUI();
+            this.updateUI();
         },
         getCurrentProjectId() {
             let body = document.querySelector('body');
@@ -30,18 +30,23 @@
 
             return body.dataset.projectId;
         },
-        refreshUI() {
-            let currentMergeRequestsIds = this.getCurrentMergeRequestsIds();
+        updateUI() {
+            let currentMergeRequestsIds = this.getCurrentMergeRequestsIdsAndSetUuidDataAttributes();
 
-            console.debug('Current merge requests IDS:', currentMergeRequestsIds);
+            console.debug('Current merge requests IDs:', currentMergeRequestsIds);
 
             this.fetchMergeRequestsDetails(currentMergeRequestsIds);
         },
-        getCurrentMergeRequestsIds() {
-            return Array.from(document.querySelectorAll('.mr-list > .merge-request .issuable-reference'))
-                .map(function(mergeRequest) {
-                    return mergeRequest.textContent.trim().replace('!', '');
-                });
+        getCurrentMergeRequestsIdsAndSetUuidDataAttributes() {
+            return Array.from(
+                document.querySelectorAll('.mr-list .merge-request')
+            ).map(function(el) {
+                let iid = el.querySelector('.issuable-reference').textContent.trim().replace('!', '');
+
+                el.dataset.iid = iid;
+
+                return iid;
+            });
         },
         fetchMergeRequestsDetails(mergeRequestIds) {
             let self = this;
@@ -57,6 +62,7 @@
                         return;
                     }
 
+                    self.removeExistingTargetBranchNodes();
                     self.handleMergeRequestsDetails(this.response);
                 } else {
                     console.error('Got error from GitLab:', this.status);
@@ -84,11 +90,28 @@
         },
         handleMergeRequestsDetails(mergeRequestsDetails) {
             mergeRequestsDetails.forEach(function(mergeRequest) {
-                console.log(
-                    mergeRequest.title,
-                    mergeRequest.source_branch,
-                    mergeRequest.target_branch
-                );
+                let branchesInfoNode = document.createElement('div');
+
+                branchesInfoNode.classList.add('issuable-info');
+                branchesInfoNode.innerHTML = '<span class="project-ref-path has-tooltip" title="Source branch">' +
+                        + '<a class="ref-name" href="#TODO">' +
+                            + '<svg class="s12 fork-sprite"><use xlink:href="/assets/icons-2206b8b510dd8a2edccabbd872bdecb67fa80554b54d351d80c5b056d048670e.svg#fork"></use></svg> ' + mergeRequest.source_branch
+                        + '</a>' +
+                    + '</span>' +
+                    + '<span class="project-ref-path has-tooltip" title="Target branch">' +
+                        + '<a class="ref-name" href="#TODO">' +
+                            + '<svg class="s12 fork-sprite"><use xlink:href="/assets/icons-2206b8b510dd8a2edccabbd872bdecb67fa80554b54d351d80c5b056d048670e.svg#fork"></use></svg> ' + mergeRequest.target_branch
+                        + '</a>' +
+                    + '</span>';
+
+                document
+                    .querySelector('.mr-list .merge-request[data-iid="' + mergeRequest.iid + '"] .issuable-main-info')
+                    .appendChild(branchesInfoNode);
+            });
+        },
+        removeExistingTargetBranchNodes() {
+            document.querySelectorAll('.mr-list .merge-request .project-ref-path').forEach(function(el) {
+                el.parentNode.removeChild(el);
             });
         }
     };
