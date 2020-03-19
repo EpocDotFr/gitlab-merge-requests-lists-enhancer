e3fc83481611ee360afb9570994f2b3bba7c38ac
Epoc <contact@epoc.fr>
2020-03-19 14:00:17 +0100

    Continuing

--- js/content.js
+++ js/content.js
@@ -1,5 +1,51 @@
 (function(globals) {
     'use strict';
 
-    console.log('Hi from content script');
+    globals.gmrle = {
+        currentProjectId: null,
+
+        init() {
+            this.currentProjectId = this.getCurrentProjectId();
+
+            if (!this.currentProjectId) {
+
+                console.error('Aborting: current project ID cannot be found');
+
+                return;
+            }
+
+            this.refreshUI();
+        },
+        getCurrentProjectId() {
+            let body = document.querySelector('body');
+
+            if (!body || !('projectId' in body.dataset)) {
+                return null;
+            }
+
+            return body.dataset.projectId;
+        },
+        refreshUI() {
+            let currentMergeRequestsIds = this.getCurrentMergeRequestsIds();
+
+            console.log(currentMergeRequestsIds);
+
+            // this.fetchMergeRequestsDetails(currentMergeRequestsIds)
+        },
+        getCurrentMergeRequestsIds() {
+            return Array.prototype.map.call(
+                document.querySelectorAll('.mr-list > .merge-request'),
+                function (mergeRequest) {
+                    return mergeRequest.dataset.id;
+                }
+            );
+        },
+        fetchMergeRequestsDetails(mergeRequestIds) {
+
+            // https://stackoverflow.com/questions/37944051/xmlhttprequest-from-firefox-webextension
+            // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+        }
+    };
+
+    globals.gmrle.init();
 }(this));
