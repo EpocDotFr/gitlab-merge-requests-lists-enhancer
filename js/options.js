5f42eca034f7aa7c249b939d938a158439047d27
Epoc <contact@epoc.fr>
2020-03-22 18:20:41 +0100

    Continuing options page

--- js/options.js
+++ js/options.js
@@ -1,5 +1,13 @@
 (function(globals) {
     'use strict';
 
+    class OptionsPage {
+        constructor() {
 
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        let op = new OptionsPage();
+    });
 }(this));
