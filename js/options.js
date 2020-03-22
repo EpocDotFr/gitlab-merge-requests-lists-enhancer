9e4211deb44081cf0b00b63bd346751fa6de9aba
Epoc <contact@epoc.fr>
2020-03-22 18:55:39 +0100

    Continuing options page with copy basic MR info option

--- js/options.js
+++ js/options.js
@@ -3,7 +3,25 @@
 
     class OptionsPage {
         constructor() {
+            this.getDomNodes();
+            this.initEventListenersOnDomNodes();
+        }
 
+        getDomNodes() {
+            this.enableButtonToCopyMrInfoCheckbox = document.querySelector('input#option_enable_button_to_copy_mr_info');
+            this.copyMrInfoFormatTextarea = document.querySelector('textarea#copy_mr_info_format');
+        }
+
+        initEventListenersOnDomNodes() {
+            let self = this;
+
+            this.enableButtonToCopyMrInfoCheckbox.addEventListener('change', function() {
+                if (this.checked) {
+                    self.copyMrInfoFormatTextarea.parentNode.parentNode.classList.remove('is-hidden');
+                } else {
+                    self.copyMrInfoFormatTextarea.parentNode.parentNode.classList.add('is-hidden');
+                }
+            });
         }
     }
 
