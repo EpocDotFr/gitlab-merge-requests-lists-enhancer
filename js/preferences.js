0acaa0769626f65262c20f9bcf6d01f501fa0bc6
Epoc <contact@epoc.fr>
2020-04-09 00:19:14 +0200

    Add comments

--- js/preferences.js
+++ js/preferences.js
@@ -6,14 +6,23 @@
             enable_buttons_to_copy_source_and_target_branches_name: true
         };
 
+        /**
+         * This class holds all the logic related to user preferences persistance.
+         */
         constructor() {}
 
+        /**
+         * Get all preferences.
+         */
         getAll(callback) {
             browser.storage.local.get(this.defaults).then(callback, function() {
                 alert('Error retrieving add-on preferences.');
             });
         }
 
+        /**
+         * Set all preferences.
+         */
         setAll(preferences) {
             browser.storage.local.set(preferences).then(function() {
                 // Do nothing if save was successful.
