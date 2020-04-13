13323ea3f49a683ead20de6d0ba186125fa7c508
Epoc <contact@epoc.fr>
2020-04-13 17:00:23 +0200

    Simplification

--- js/content.js
+++ js/content.js
@@ -221,7 +221,7 @@
                     '</button> ';
 
                     this.parseHtmlAndPrepend(
-                        mergeRequestContainer.querySelector('.issuable-reference').parentNode,
+                        mergeRequestContainer.querySelector('.issuable-info'),
                         copyMrInfoButton
                     );
                 }
@@ -265,8 +265,7 @@
         }
 
         /**
-         * Sets several data-* attributes on a DOM node representing a Merge Request from the GitLab API response so
-         * these values may be used later.
+         * Sets several data-* attributes on a DOM node representing a Merge Request so these values may be used later.
          */
         setDataAttributesToMergeRequestContainer(mergeRequestContainer, mergeRequest) {
             mergeRequestContainer.dataset.title = mergeRequest.title;
